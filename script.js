864e70ec16115fd2ebd0bfcbb783a9bcff8ed4bc
maxconnors52 <maxconnors52@gmail.com>
2016-02-28 14:54:58 -0500

    New JS
    alerts

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-alert("Thanks for visiting my site. That was your first mistake. LOLZ.");
+// window.onload = function(e){
+
+
+alert("Thanks for visiting my site. That was your first mistake. Lolz.");
+var kittens;
+kittens = 5;
+
+function turtleFact () {
+alert("Some turtles can live longer than humans.");
+}
+
+// turtleFact();
+
+function temptGang(gangName) {
+	alert(gangName + " Come out and play!")
+}
+
+function addNumbers(num1, num2) {
+	alert(num1+num2)
+}
+
+// addNumbers(10, 5);
+
+function square(number) {
+	return number * number;
+}
+
+function displaySquare(squaredNumber) {
+	console.log("Your number is " + squaredNumber);
+}
+
+// displaySquare(square(3));
+
+// var pageNode = document.getElementsByTagName('body')[0];
+// pageNode.innerHTML = '<h1>Oh Noes!</h1> <p>I just changed the whole page!</p>'
+
+var title = document.getElementById('open-title');
+title.innerHTML = "Thanks for visiting.";
+
+}
+
